7c4cc5d329773e67b974b764536ed99a301cf5ca
Dame <camaradame@gmail.com>
2018-09-03 00:00:36 +0000

    page commande

--- src/providers/gloabal-variable/gloabal-variable.ts
+++ src/providers/gloabal-variable/gloabal-variable.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class  GlobalVariableProvider {
 
-  public URL ="http://services.ajit.sn/ws/";
+  public URL ="http://services.ajit.sn/ws/client/";
 
 }
